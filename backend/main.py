eef89581619ce2f5f247d0492d836209af4d6cd4
drako9159 <chochito9159@gmail.com>
2023-07-07 21:38:53 -0600

    configure routes

--- backend/main.py
+++ backend/main.py
@@ -10,3 +10,21 @@ async def get_tasks():
     return "all tasks"
 
 
+@app.post("/api/tasks")
+async def create_tasks():
+    return "create task"
+
+
+@app.get("/api/tasks/{id}")
+async def get_tasks():
+    return "single task"
+
+
+@app.put("/api/tasks/{id}")
+async def update_task():
+    return "update task"
+
+@app.delete("/api/tasks/{id}")
+async def delete_task():
+    return "delete task"
+
